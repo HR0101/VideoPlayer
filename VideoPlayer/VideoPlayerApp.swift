dda29392020bf6ff69d5b5adef218c73af4e35bc
HR0101 <hararyuto0101@gmail.com>
2025-06-16 15:17:25 +0900

    Initial Commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by hara ryuto   on 2025/06/16.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VideoPlayerTests/VideoPlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerTests.swift
+//  VideoPlayerTests
+//
+//  Created by hara ryuto   on 2025/06/16.
+//
+
+import Testing
+@testable import VideoPlayer
+
+struct VideoPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
